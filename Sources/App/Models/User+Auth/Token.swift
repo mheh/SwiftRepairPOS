f2f2f05327cd7a4af82849a92c1f9f25d794cc4e
mheh <mhehmsoth@gmail.com>
2024-12-05 21:11:23 -0600

    JWT payload and UserAuthenticator setup

---
+++ Sources/App/Models/User+Auth/Token.swift
@@ -0,0 +1,40 @@
+//
+//  Token.swift
+//  SwiftRepairPOS
+//
+//  Created by Milo Hehmsoth on 12/5/24.
+//
+
+import Vapor
+import JWT
+
+/// The `access token` model sent to the frontend. Contains the `User` information for quicker access.
+struct Payload: JWTPayload, Authenticatable {
+    /// The user
+    var userID: UUID
+    var username: String
+    var fullName: String
+    var email: String
+    var isAdmin: Bool
+    var exp: ExpirationClaim
+    
+    func verify(using signer: JWTSigner) throws {
+        try self.exp.verifyNotExpired()
+    }
+    
+    /// Create a new `access token` for the provided user
+    init(user: User) throws {
+        self.userID =       try user.requireID()
+        self.username =     user.username
+        self.fullName =     user.fullName
+        self.email =        user.email
+        self.isAdmin =      user.isAdmin
+        self.exp = ExpirationClaim(value: Date().addingTimeInterval(CONSTANT_TOKEN_ACCESS_LIFETIME))
+    }
+}
+
+struct UserAuthenticator: AsyncJWTAuthenticator {
+    func authenticate(jwt: Payload, for request: Vapor.Request) async throws {
+        request.auth.login(jwt)
+    }
+}
---
+++ Sources/App/constants.swift
@@ -0,0 +1,11 @@
+//
+//  constants.swift
+//  SwiftRepairPOS
+//
+//  Created by Milo Hehmsoth on 12/5/24.
+//
+
+/// 3 hours
+let CONSTANT_TOKEN_REFRESH_LIFETIME: Double = 60 * 60 * 3
+/// 30 minutes
+let CONSTANT_TOKEN_ACCESS_LIFETIME: Double = 60 * 30
