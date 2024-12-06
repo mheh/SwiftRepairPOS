2f5e7a254f4856d1ec95ee6d5bcef158d0a47746
mheh <mhehmsoth@gmail.com>
2024-12-05 20:43:55 -0600

    Add JWT config

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -2,12 +2,19 @@ import NIOSSL
 import Fluent
 import FluentPostgresDriver
 import Vapor
+import JWT
 
 // configures your application
 public func configure(_ app: Application) async throws {
     // uncomment to serve files from /Public folder
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
 
+    // configure JWT
+    guard let jwks = FileManager.default.contents(atPath: app.directory.workingDirectory + (Environment.get("JWKS_KEYPAIR_FILE") ?? "keypair.jwks")),
+          let jwksString = String(data: jwks, encoding: .utf8)
+    else { fatalError("Failed to load JWKS Keypair file.") }
+    try app.jwt.signers.use(jwksJSON: jwksString)
+    
     app.databases.use(DatabaseConfigurationFactory.postgres(configuration: .init(
         hostname: Environment.get("DATABASE_HOST") ?? "localhost",
         port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? SQLPostgresConfiguration.ianaPortNumber,
