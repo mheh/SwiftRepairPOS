78f9ff2630cf2be75195aff92a9cbe3d5237c013
mheh <mhehmsoth@gmail.com>
2024-12-06 18:52:39 -0600

    Register the authcontroller

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,5 +1,13 @@
+//
+//  routes.swift
+//  SwiftRepairPOS
+//
+//  Created by Milo Hehmsoth on 12/6/24.
+//
+
 import Fluent
 import Vapor
 
 func routes(_ app: Application) throws {
+    try app.routes.register(collection: AuthController())
 }
