d53691075cbf0153acf2f531156246c86aeeb089
mheh <mhehmsoth@gmail.com>
2024-12-06 19:51:57 -0600

    gen-db script didn't work. Updated

--- scripts/gen-db.sh
+++ scripts/gen-db.sh
@@ -7,15 +7,18 @@
 #
 
 ## Gather .env variables
-ENVFILE=./swiftrepairpos/.env
+ENVFILE=./.env
 if [ -f "$ENVFILE" ]
 then
   export $(cat $ENVFILE | sed 's/#.*//g' | xargs)
+else
+  echo "No .env file found, aborting... Run this script in the working directory of this package."
+  exit
 fi
 
 docker run -d --name swift-repair-pos-test \
-  -e POSTGRES_USER=$(echo $POSTGRES_USER) -e POSTGRES_PASSWORD=$(echo $POSTGRES_PASSWORD) \
+  -e POSTGRES_USER=$(echo $DATABASE_USERNAME) -e POSTGRES_PASSWORD=$(echo $DATABASE_PASSWORD) \
   -p 5433:5432 postgres:latest -c log_statement=all
 docker run -d --name swift-repair-pos \
-  -e POSTGRES_USER=$(echo $POSTGRES_USER) -e POSTGRES_PASSWORD=$(echo $POSTGRES_PASSWORD) \
+  -e POSTGRES_USER=$(echo $DATABASE_USERNAME) -e POSTGRES_PASSWORD=$(echo $DATABASE_PASSWORD) \
   -p 5432:5432 postgres:latest -c log_statement=all
